4833e024d14de2a69d562f1ed3389f907b68a0c4
Mrak9087 <maks19-88@mail.ru>
2023-08-21 18:22:08 +0400

    feat: change hook useInterval

--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -1,22 +1,19 @@
 import { useEffect, useRef } from 'react';
 
 export function useInterval(callback: () => void, delay: number | null) {
-  const savedCallback = useRef<null | (() => void)>(null);
+  const savedCallback = useRef(callback)
 
   useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+    savedCallback.current = callback
+  }, [callback])
 
   useEffect(() => {
-    function tick(): void {
-      if (savedCallback.current) savedCallback.current();
+    if (!delay && delay !== 0) {
+      return
     }
 
-    if (delay !== null) {
-      const id = setInterval(tick, delay);
-      return () => {
-        clearInterval(id);
-      };
-    }
-  }, [delay]);
+    const id = setInterval(() => savedCallback.current(), delay)
+
+    return () => clearInterval(id)
+  }, [delay])
 }
