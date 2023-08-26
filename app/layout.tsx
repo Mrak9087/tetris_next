8c8ae7254cce624bf9c7bfc09edaf01014f2c56b
Mrak9087 <maks19-88@mail.ru>
2023-08-26 18:19:31 +0400

    feat: add header and page control

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 import type { Metadata } from 'next'
 
@@ -14,7 +15,25 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+      <header className="w-full flex justify-end pr-5 pt-2 gap-2 text-sm lg:text-lg font-bold bg-black border-b-gray-400 border-b-2">
+          <Link
+            href="/"
+            scroll={false}
+            className="hover:underline tracking-wider px-5 py-2 bg-gray-400 rounded-t-xl"
+          >
+            Тетрис
+          </Link>
+          <Link
+            href="/control"
+            scroll={false}
+            className="hover:underline tracking-wider px-5 py-2 bg-gray-400 rounded-t-xl"
+          >
+            Управление
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
