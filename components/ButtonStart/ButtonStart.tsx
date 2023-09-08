fdec55d3d5ca2b9494335884d9ee49c4c03e8deb
Mrak9087 <maks19-88@mail.ru>
2023-09-08 14:01:48 +0400

    feat: add styles to ButtonStart

--- components/ButtonStart/ButtonStart.tsx
+++ components/ButtonStart/ButtonStart.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 interface IButtonStart {
@@ -6,12 +7,17 @@ interface IButtonStart {
 
 function ButtonStart({ callback }: IButtonStart) {
   return (
-    <button
-      className="outline-none border-2 rounded-lg text-xs md:text-sm lg:text-base xl:text-xl font-bold bg-black tracking-widest px-5 py-3 text-gray-400 "
+    <div
+      className={clsx(
+        "outline-none rounded-lg text-xs md:text-sm lg:text-base xl:text-xl font-bold bg-black tracking-widest  text-gray-400 relative block cursor-pointer p-[2px] hover:before:rotate-180 overflow-hidden ",
+        'before:content-[" "] before:absolute before:top-[-2px] before:right-[-2px] before:bottom-[-2px] before:left-[-2px] before:z-0 before:pointer-events-none before:bg-gradient-to-tl before:from-sky-500 before:to-violet-400 before:transition-all before:ease-linear before:duration-1000'
+      )}
       onClick={callback}
     >
-      Старт
-    </button>
+      <div className="bg-black h-full w-full rounded-lg flex items-center justify-center px-5 py-3 relative pointer-events-none">
+        Старт
+      </div>
+    </div>
   );
 }
 
